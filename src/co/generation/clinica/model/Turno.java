c84f43895ec87001d16816439d97409bc1bbb02b
Mauricio <297007275+MarAlvGEN@users.noreply.github.com>
2026-08-24 11:26:27 -0500

    TURNOS (#2)
    Falta equals, pero se hará una vez se tengan las otras clases.

--- src/co/generation/clinica/model/Turno.java
+++ src/co/generation/clinica/model/Turno.java
@@ -1,4 +1,86 @@
 package co.generation.clinica.model;
 
+import java.time.LocalDateTime;
+
 public class Turno {
+    private int id;
+    private Paciente paciente;
+    private Medico medico;
+    private LocalDateTime fechaHora;
+    private EstadoTurno estado;
+
+    public Turno(int id, Paciente paciente, Medico medico, LocalDateTime fechaHora, EstadoTurno estado) {
+        this.id = id;
+        setPaciente(paciente);
+        setMedico(medico);
+        setFechaHora(fechaHora);
+        setEstado(estado);
+    }
+
+    public Turno(Paciente paciente, Medico medico, LocalDateTime fechaHora) {
+        setPaciente(paciente);
+        setMedico(medico);
+        setFechaHora(fechaHora);
+        this.estado = EstadoTurno.PENDIENTE;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public EstadoTurno getEstado() {
+        return estado;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        if (paciente == null) {
+            throw new IllegalArgumentException("El paciente no puede estar vacio.");
+        }
+        this.paciente = paciente;
+    }
+
+    public void setMedico(Medico medico) {
+        if (medico == null) {
+            throw new IllegalArgumentException("El médico no puede estar vacio.");
+        }
+        this.medico = medico;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        if (fechaHora == null) {
+            throw new IllegalArgumentException("La fecha y hora no pueden estar vacias.");
+        }
+        this.fechaHora = fechaHora;
+    }
+
+    public void setEstado(EstadoTurno estado) {
+        if (estado == null) {
+            throw new IllegalArgumentException("El estado no puede estar vacio.");
+        }
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + estado + "] " +
+                paciente.getNombre() + " " + paciente.getApellido() + " - " +
+                medico.getNombre() + " " + medico.getApellido() + " (" + medico.getEspecialidad() + ") - " +
+                fechaHora;
+    }
 }
